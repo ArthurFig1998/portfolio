fa4667b3aecc387d7cffb65d3450cda33714cf00
Arthur Figueiredo <38699297+ArthurFig1998@users.noreply.github.com>
2018-06-19 17:51:28 -0600

    finishing up

--- floralJS.js
+++ floralJS.js
@@ -27,10 +27,14 @@ function checkPet()
         petYes.style.display = "none";    
 }
 
-function calcEstimate()
+function calcEstimate(event)
 {
+    event.preventDefault();
+    console.log(event);
     var dollarsPerHour = 24;
-    var estimatedTime = 0;
+    var estimatedTime = 4;
+    var form = event.target;
+    form.style.display = "none";
     
     
     ///TYPE OF CLEANING
@@ -43,47 +47,6 @@ function calcEstimate()
     var onceThreeMonths = document.getElementById('onceThreeMonths');
     ///
     
-    ///LAST CLEANING
-    var lessThanMonth = document.getElementById('lessMonth');
-    var betweenMonthTwo = document.getElementById('betweenMonthTwo');
-    var threeMore = document.getElementById('threeMore');
-    ///
-    
-    
-    
-    //// HOUSE SQ FOOTAGE
-//    var 400700 = document.getElementById('4-7');
-//    var 701-1000 = document.getElementById('7-10');
-//    var 1001-1300 = document.getElementById('10-13');
-//    var 1301-1700 = document.getElementById('13-17');
-//    var 1701-2100 = document.getElementById('17-21');
-//    var 2101-2500 = document.getElementById('21-25');
-//    var 2501-2900 = document.getElementById('25-29');
-//    var 2901-3300 = document.getElementById('29-33');
-//    var 33+ = document.getElementById('33+');
-    ////
-    
-    
-    /******************************************************
-    Adding estimated time of service based on last cleaning
-    *******************************************************/
-    if(lessThanMonth.checked)
-        {
-            estimatedTime += 0;
-        }
-    else if(betweenMonthTwo.checked)
-        {
-            estimatedTime += 2;
-        }
-    else if(threeMore.checked)
-        {
-            estimatedTime += 4;
-        }
-    
-    /*****************
-    *****************/
-    
-    
     /****************
     Defining pricing
     ****************/
@@ -92,32 +55,81 @@ function calcEstimate()
             dollarsPerHour = 27;
             estimatedTime += 12;
         }
-    else
-        {
-            estimatedTime = 0;
-        }
+   
     
     if(onceWeek.checked)
-        {
             dollarsPerHour = 20;
-        }
     else if(onceTwoWeeks.checked)
-        {
             dollarsPerHour = 21;
-        }
     else if(onceMonth.checked)
-        {
             dollarsPerHour = 22;
-        }
     else if(onceTwoMonths.checked)
-        {
             dollarsPerHour = 23;
-        }
     else if(onceThreeMonths.checked)
-        {
             dollarsPerHour = 24;
-        }
+    
+    
+    ///LAST CLEANING
+    var lessThanMonth = document.getElementById('lessMonth');
+    var betweenMonthTwo = document.getElementById('betweenMonthTwo');
+    var threeMore = document.getElementById('threeMore');
+    ///
+    
+     /******************************************************
+    Adding estimated time of service based on last cleaning
+    *******************************************************/
+    if(lessThanMonth.checked)
+            estimatedTime += 0;
+    else if(betweenMonthTwo.checked)
+            estimatedTime += 2;
+    else if(threeMore.checked)
+            estimatedTime += 4;
+    
+    
+    //// HOUSE SQ FOOTAGE
+    var smallish = document.getElementById('4-7');
+    var small = document.getElementById('7-10');
+    var mediumish = document.getElementById('10-13');
+    var medium = document.getElementById('13-17');
+    var largeish = document.getElementById('17-21');
+    var large = document.getElementById('21-25');
+    var bigish = document.getElementById('25-29');
+    var big = document.getElementById('29-33');
+    var bigger = document.getElementById('33+');
+    ///
+    
+    /**********************************
+    Adding time according to house size
+    **********************************/
+    if(smallish.checked)
+        estimatedTime += 1;
+    else if(small.checked)
+        estimatedTime += 2;
+    else if(mediumish.checked)
+        estimatedTime += 3;
+    else if(medium.checked)
+        estimatedTime += 4;
+    else if(largeish.checked)
+        estimatedTime += 5;
+    else if(large.checked)
+        estimatedTime += 6;
+    else if(bigish.checked)
+        estimatedTime += 7;
+    else if(big.checked)
+        estimatedTime += 8;
+    else if(bigger.checked)
+        estimatedTime += 9;
+    
+    
+    var finalPrice = estimatedTime * dollarsPerHour;
+    
+    document.getElementById('finalMessage').style.display = "block";
+    document.getElementById('finalPrice').innerHTML = finalPrice;
     
     
     console.log(estimatedTime, dollarsPerHour);
+    
+    
 }
+
+document.getElementById('formTesting').addEventListener("submit", calcEstimate);
